ec532e1245e31a0db40c42b73379228a9e3b9c85
Nandakishore VV <vnandub@gmail.com>
2022-01-16 01:05:43 +0530

    contacts added, passing props from child to parent

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import './App.css';
 import Header from "./Header"
 import ContactList from "./ContactList"
@@ -6,22 +6,15 @@ import ContactCards from "./ContactCards"
 import AddContact from "./AddContact"
 
 function App() {
-  const contacts = [
-    {
-      id: "1",
-      name: "Asahi",
-      email: "asai@nop.com"
-    },
-    {
-      id: "2",
-      name: "Kageyama",
-      email: "kag@nop.com"
-    }
-  ];
+  const [contacts, setContacts] = useState([])
+
+  const addContactHandler = (contact) =>{
+    setContacts([...contacts, contact])
+  }
   return (
     <div className="ui container">
       <Header />
-      <AddContact />
+      <AddContact addContactHandler = {addContactHandler}/>
        <ContactList contacts = {contacts}/>
     </div>
     
--- src/Components/ContactList.js
+++ src/Components/ContactList.js
@@ -1,18 +1,11 @@
 import React from "react";
+import ContactCards from "./ContactCards";
 
 const ContactList = (props) =>{
     console.log(props)
     const renderContactList = props.contacts.map((contacts) => {
         return(
-            <div className="item">
-                <div className="content">
-                    <div className="header">
-                        {contacts.name}
-                    </div>
-                    <div>{contacts.email}</div>
-                </div>
-                <i className="trash alternate outline icon"></i>
-            </div>
+           <ContactCards contacts = {contacts}></ContactCards>
         )
     })
     return (
